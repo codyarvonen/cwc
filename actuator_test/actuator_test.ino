dcb402b6f640213efb2eda4aa3480ae6eaa4779a
codyarvonen <codyarvonen@gmail.com>
2022-09-28 10:01:27 -0600

    fix bugs

--- actuator_test/actuator_test.ino
+++ actuator_test/actuator_test.ino
@@ -1,6 +1,5 @@
 // https://github.com/arduino-libraries/Servo
 #include <Servo.h>
-
 #define ACTUATOR_PIN 12
 
 Servo pitchControl;
@@ -11,18 +10,43 @@ void setup() {
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        int pitch = atoi(Serial.readString());
-        // int pitch = Serial.read();
-        char temp[8];
-        sprinf(temp, "%d\n", pitch);
-        Serial.write(temp);
-        if (pitchControl.attached()) {
-            set_pitch(pitch);
-        } else {
-            Serial.println("Actuator is not attached.");
-        }
-    }
+    // if (Serial.available() > 0) {
+    //     // char *input = Serial.readString();
+    //     // int pitch = atoi(input);
+    //     int pitch = Serial.read();
+    //     // char temp[8];
+    //     // sprinf(temp, "%d\n", pitch);
+    //     // Serial.write(temp);
+    //     if (pitchControl.attached()) {
+    //         if (pitch == '0') {
+    //           Serial.println("0");
+    //             set_pitch(0);
+    //         }
+    //         if (pitch == '1') {
+    //           Serial.println("50");
+    //             set_pitch(50);
+    //         }
+    //         if (pitch == '2') {
+    //           Serial.println("100");
+    //             set_pitch(100);
+    //         } else {
+    //           Serial.println("150");
+    //             set_pitch(150);
+    //         }
+    //     } else {
+    //         Serial.println("Actuator is not attached.");
+    //     }
+    // }
+
+    // TODO: SET BOUNDARY MAX AND MIN FOR ACTUATOR
+
+    set_pitch(0);
+    set_pitch(50);
+    set_pitch(100);
+    set_pitch(150);
+    set_pitch(100);
+    set_pitch(50);
+    set_pitch(0);
 }
 
 void set_pitch(int pitch_angle) {
