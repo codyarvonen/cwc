081c527935c8525903e33f0c4b2a89bd5a377215
codyarvonen <codyarvonen@gmail.com>
2022-09-29 10:12:04 -0600

    update reading from serial monitor

--- actuator_test/actuator_test.ino
+++ actuator_test/actuator_test.ino
@@ -1,6 +1,12 @@
 // https://github.com/arduino-libraries/Servo
 #include <Servo.h>
+#include <math.h>
+
 #define ACTUATOR_PIN 12
+#define MAX_PITCH_CHARS 4
+#define MAX_PITCH_SIZE 3
+
+int digits = 0;
 
 Servo pitchControl;
 
@@ -10,43 +16,43 @@ void setup() {
 }
 
 void loop() {
-    // if (Serial.available() > 0) {
-    //     // char *input = Serial.readString();
-    //     // int pitch = atoi(input);
-    //     int pitch = Serial.read();
-    //     // char temp[8];
-    //     // sprinf(temp, "%d\n", pitch);
-    //     // Serial.write(temp);
-    //     if (pitchControl.attached()) {
-    //         if (pitch == '0') {
-    //           Serial.println("0");
-    //             set_pitch(0);
-    //         }
-    //         if (pitch == '1') {
-    //           Serial.println("50");
-    //             set_pitch(50);
-    //         }
-    //         if (pitch == '2') {
-    //           Serial.println("100");
-    //             set_pitch(100);
-    //         } else {
-    //           Serial.println("150");
-    //             set_pitch(150);
-    //         }
-    //     } else {
-    //         Serial.println("Actuator is not attached.");
-    //     }
-    // }
+
+    char pitches[MAX_PITCH_CHARS];
+
+    while (Serial.available() > 0) {
+        Serial.println("Getting digit");
+        if (digits < MAX_PITCH_CHARS) {
+            pitches[digits] = Serial.read();
+            digits++;
+        }
+    }
+    if (digits > 0) {
+        Serial.println(digits);
+    }
+    if (digits > 1) {
+        Serial.println(digits);
+        for (int i = 0; i < digits; i++) {
+            Serial.println(pitches[i]);
+        }
+        int pitch = (pitches[0] - 48) * pow(10, (digits - 2));
+        Serial.println(pitch);
+        if (digits > 2) {
+            pitch += (pitches[1] - 48) * pow(10, (digits - 3));
+        }
+        Serial.println(pitch);
+        if (digits > 3) {
+            pitch += (pitches[2] - 48) * pow(10, (digits - 4));
+        }
+        Serial.println(pitch);
+        if (pitchControl.attached()) {
+            set_pitch(pitch);
+        } else {
+            Serial.println("Actuator is not attached.");
+        }
+        digits = 0;
+    }
 
     // TODO: SET BOUNDARY MAX AND MIN FOR ACTUATOR
-
-    set_pitch(0);
-    set_pitch(50);
-    set_pitch(100);
-    set_pitch(150);
-    set_pitch(100);
-    set_pitch(50);
-    set_pitch(0);
 }
 
 void set_pitch(int pitch_angle) {
