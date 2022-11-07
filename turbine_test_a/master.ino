7666f2be92b9d4dab5cba6cfde2fb5648b9cb399
brycemr <brycerichard26@gmail.com>
2022-11-07 09:47:24 -0700

    Update master.ino

--- turbine_test_a/master.ino
+++ turbine_test_a/master.ino
@@ -10,8 +10,12 @@
 #define ENGAGED_BRAKE_ANGLE 90
 #define DISENGAGED_BRAKE_ANGLE 110
 #define ACTUATOR_MAX 100
-#define ACTUATOR_MIN 40
+#define ACTUATOR_MIN 0
 #define LOAD_DISCONNECT_THRESHOLD 0.1
+#define INITIAL_PITCH 30 // This value is arbitrary, needs to be tested
+#define BRAKE_PITCH 0 // This value is arbitrary, needs to be tested
+#define PRIMARY_SWITCH_SIGNAL_PIN 10 // This value is arbitrary and needs to be set
+#define SECONDARY_SWITCH_SIGNAL_PIN 11 // This value is arbitrary and needs to be set
 
 bool brakeIsEngaged = false;
 
@@ -33,6 +37,11 @@ enum states_t {
     testing
 } currentState;
 
+enum states_t {
+  primary_switch_state,
+  secondary_switch_state
+} switchState;
+
 typedef enum {
     pitch_test,
     brake_test,
@@ -47,11 +56,14 @@ void setup() {
     Serial.write("Beginning test! \n");
     pitchControl.attach(ACTUATOR_PIN);
     brakeControl.attach(BRAKE_SERVO_PIN);
+    pinMode(PRIMARY_SWITCH_SIGNAL_PIN, OUTPUT);
+    pinMode(SECONDARY_SWITCH_SIGNAL_PIN, OUTPUT);
 }
 
 // unsigned int integerValue = 0;
 // char incomingByte;
 int pitch;
+bool restartProtocolComplete = false;
 
 void loop() {
 
@@ -68,37 +80,50 @@ void loop() {
         if (toggle_test()) {
             currentState = testing;
         }
-        // set pitch to startup angle, trigger switches back
+        else if (restartProtocolComplete) {
+            restartProtocolComplete = false;
+            currentState = power_curve;
+        }
         break;
     case power_curve:
         if (toggle_test()) {
             currentState = testing;
         }
-        /* Figure out how to determine wind speed
-        if(avgWindSpeed >= 11 m/s){
+        // Figure out how to determine wind speed, lookup table or windspeed sensor
+        if(avgWindSpeed >= 11){
             currentState = steady_power;
         }
-        */
-        check_E_Stop();
+        if(engage_E_Stop()){
+          currentState = emergency_shutdown;
+        }
         break;
     case steady_power:
         if (toggle_test()) {
             currentState = testing;
         }
-        /* Figure out how to determine wind speed
-        if(avgWindSpeed > 14 m/s){
+        // Figure out how to determine wind speed, lookup table or windspeed sensor
+        if(avgWindSpeed > 14){
             currentState = survival;
         }
-        */
-        check_E_Stop();
+        else if(avgWindSpeed < 11){
+          currentState = steady_power;
+        }
+        if(engage_E_Stop()){
+          currentState = emergency_shutdown;
+        }
         break;
     case survival:
         if (toggle_test()) {
             currentState = testing;
         }
         // Set load and pitch to values that are best fit for survival
-        // same as E_Stop but leave switches in intial position?
-        check_E_Stop();
+        // same as E_Stop but leave switches in initial position?
+        if(avgWindSpeed < 14){
+          currentState = steady_power;
+        }
+        if(engage_E_Stop()){
+          currentState = emergency_shutdown;
+        }
         break;
     case emergency_shutdown:
         if (toggle_test()) {
@@ -123,24 +148,56 @@ void loop() {
     // Pitch blades out to inital state, trigger switches back so generator powers
     // nacelle components and the load
     case restart:
+        set_pitch(INITIAL_PITCH)
+        if(currentPitch == INITIAL_PITCH){
+          set_switches(true);
+          restartProtocolComplete = true;
+        }
         break;
 
     // Adjust the load to reach maximum Cp at each wind speed 5-11 m/s
+    // TODO: test ideal resistor values for varying wind speeds and fixed pitch angles
     case power_curve:
-        break;
+      set_load(8); // set this load based on current power Cp? Current wind speed? current rpm?
+      // TODO: Create function that uses lookup table(s) to determine and set resistor value
+      break;
 
     // Adjust the pitch to maintain constant power output from 11-14 m/s
+    // TODO: test pitch angles to have a fixed rpm for varying wind speeds and set resistor value
     case steady_power:
+        set_load(STEADY_POWER_RESISTOR); // move this so it is set right before moving to steady_power?
+        curr_rpm = encoder(); // Get a rolling average instead of single data point
+        if (curr_rpm < STEADY_POWER_RPM - STEADY_POWER_RANGE){
+          currentPitch = pitchControl.read() + 5; // Find a good step size
+          set_pitch(currentPitch); // set this pitch based on current rpm
+        }
+        else if (curr_rpm > STEADY_POWER_RPM + STEADY_POWER_RANGE){
+          currentPitch = pitchControl.read() - 5; // Find a good step size
+          set_pitch(currentPitch); // set this pitch based on current rpm
+        }
+        
         break;
 
     // Adjust pitch and load to minimize rpm and stress in the system
     case survival:
         break;
 
-    // Flip switches so components draw from the wall and the generator runs
-    // to the brake circuit
+    // After pitch is set to brake state, Flip switches so components draw from the wall and the generator runs
+    // to the brake circuit. Hold this state until button is disengaged, or load reconnected.
     case emergency_shutdown:
-        break;
+    
+      // Set switches to secondary state, opens load circuit, and powers the nacelle from the wall
+      set_pitch(BRAKE_PITCH);
+
+      // When we no longer need control, switch to run generator to brake circuit and prepare to draw power from wall for restart
+      // check switch state so we only make the switch once, otherwise results in constant switch opening and closing
+      if(currentPitch == BRAKE_PITCH && switchState == primary_switch_state){ 
+        set_switches(false);
+      }
+      if(!engage_E_Stop()){
+        currentState = restart;
+      } 
+      break;
     }
 
     /********************/
@@ -199,6 +256,27 @@ void engageBrake() { brakeControl.write(ENGAGED_BRAKE_ANGLE); }
 
 void disengageBrake() { brakeControl.write(DISENGAGED_BRAKE_ANGLE); }
 
+void set_switches(bool initialState){
+  /* The initial state has the load switch and generator to nacelle switch closed, and the wall to nacelle switch and brake switch open.
+  The generator to nacelle switch and the load switch share a digital signal. The wall to nacelle switch and the brake switch
+  also share a digital signal. */
+
+  //First set all switches to open, to avoid ever having all switches closed
+  digitalWrite(PRIMARY_SWITCH_SIGNAL_PIN, LOW);
+  digitalWrite(SECONDARY_SWITCH_SIGNAL_PIN, LOW);
+  if(initialState){
+    digitalWrite(PRIMARY_SWITCH_SIGNAL_PIN, HIGH);
+  }
+  else{
+    digitalWrite(SECONDARY_SWITCH_SIGNAL_PIN, HIGH);
+  }
+}
+
+void set_load(int resistorValue){
+  // TALK WITH POWER TO SEE WHAT SWITCHES THEY NEED AND HOW THE 
+  // LOAD WILL BE IMPLEMENTED
+}
+
 float encoder() {
     static long oldPosition = 0;
     static long oldtime = 0;
@@ -218,7 +296,7 @@ float encoder() {
     return rpm;
 }
 
-void check_E_Stop() {
+void engage_E_Stop() {
     int value;
     float volt;
 
@@ -231,8 +309,15 @@ void check_E_Stop() {
     Serial.print("  Volt: ");
     Serial.println(volt);
     if (value < LOAD_DISCONNECT_THRESHOLD) {
-        Serial.println("NO LOAD DETECTED, BEGIN E-STOP");
-        currentState = emergency_shutdown;
+      Serial.println("NO LOAD DETECTED, BEGIN E-STOP");
+      return true;
+    }
+    else if (estop_button_pressed){
+      Serial.println("ESTOP BUTTON ENGAGED, BEGIN E-STOP");
+      return true;
+    }
+    else {
+      return false;
     }
 }
 
